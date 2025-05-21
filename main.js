10cdcaaab4ae9a3b5b2a54158f6d649c5ebfc302
rabbitCase <gnair3004@gmail.com>
2025-05-21 20:32:22 +0530

    modularised and cleaned up code

--- main.js
+++ main.js
@@ -1,84 +1,121 @@
 import {list} from './wordlist.js';
 
 const word = Array.from(list[Math.floor(Math.random() * list.length)].toUpperCase());
-console.log("selected word: ",word);
 
 let checkpoint = [5,10,15,20,25,30];
-let checked = [];
+let checked = [];//if index is included in a checked word, disable backspace for that index and all indices preceeding it
 
 const gameContainer = document.getElementById('game-container');
-for(let i=0;i<6;i++){
+
+for(let i=0;i<6;i++){//create grid for the game
     const boxContainer = document.createElement('div');
     boxContainer.className = "row-container";
     gameContainer.appendChild(boxContainer);
     for(let j=0;j<5;j++){
         const box = document.createElement('input');
+        box.disabled = true;
         box.className = "row";
         box.style.transitionDelay = `${0.2 * j}s`;
         boxContainer.appendChild(box);
-        // box.addEventListener("transitionend", (event) => {
-        //     if (event.propertyName === "background-color") { 
-        //         box.style.transform = 'translateX(20px)';
-        //     }
-        // });
+    }
+}
+
+const divs = document.querySelectorAll('.row');//returns nodelist
+let i = 0;//current character in divs
+let isChecked = false;//if 5 letters are entered and checked
+let stopFlag = false;//stop the game
+let iteration = 0;//total chances completed
+
+document.addEventListener('keydown', (event) => {//for keyboard inputs
+    if(stopFlag){
+        return;
+    }
+
+    else if (!/^[a-zA-Z]$/.test(event.key)) {//only a-z allowed
+        return;
+    }
+
+    else if(checkpoint.includes(i) && !isChecked){//five characters are already entered...
+        handleEnter();
+    }
+
+    else{
+        handleKeyPress(event.key.toUpperCase());
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'Backspace'){
+        handleBackspace();
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if(event.key == 'Enter' && checkpoint.includes(i) && !isChecked){
+        handleEnter();
+    }
+});
+
+document.getElementById('backspace').addEventListener('click', () =>{
+    handleBackspace();
+});
+
+document.getElementById('enter').addEventListener('click', () =>{
+    handleEnter();
+})
+
+document.querySelectorAll('#keyboard > div').forEach(div => {
+    div.addEventListener('click', (event) => {
+        if(event.target.className == 'letter'){
+            if(stopFlag){
+                return;
+            }
+
+            else if(checkpoint.includes(i) && !isChecked){
+                handleEnter();
+            }
+
+            else{
+                handleKeyPress(event.target.textContent.toUpperCase());
+            }
+        } 
+    });
+})
+
+function handleKeyPress(letter){
+    divs[i].value = `${letter}`; 
+    i++;
+    isChecked = false;
+}
+
+function handleEnter(){
+    if(checkpoint.includes(i) && !isChecked){
+        if(findWord(i-5)){
+            wordCheck(i-5);
+            isChecked = true;
+            checked.push(checkpoint.shift());
+            return;
+        }
+        else{
+            alert("Word not in word list");
+        }
     }
     
 }
 
-const divs = document.querySelectorAll('.row');
-let i = 0;
-let isChecked = false;
-let stopFlag = false;
-let iteration = 0;
-document.addEventListener('keydown', (event) => {
-    if(stopFlag){
-        return;
-    }
-    if(event.key == 'Backspace'){
-        if(checked.includes(i) || i == 0){
+function handleBackspace(){
+    if(checked.includes(i) || i == 0){
             return;
-        }
-        divs[i-1].value = "";
-        i--;
-        return;
     }
-    else if(event.key == 'Enter' && checkpoint.includes(i) && !isChecked){
-        if(findword(i-5)){
-            wordcheck(i-5);
-            isChecked = true;
-            checked.push(checkpoint.shift());
-            return;
-        }
-        else{
-            alert("Word not in word list");
-        }
-
-    }
-    else if (!/^[a-zA-Z]$/.test(event.key)) {//only a-z allowed
-        return;
-    }
-    else if(checkpoint.includes(i) && !isChecked){//five characters are already entered...
-        if(findword(i-5)){//...and the word exists in the list
-            wordcheck(i-5);//then check the word instead of accepting another character
-            isChecked = true;
-            checked.push(checkpoint.shift());
-            return;
-        }
-        else{
-            alert("Word not in word list");
-        }
-    }
-    else{
-        divs[i].value = event.key.toUpperCase(); 
-        i++;
-        isChecked = false;
-    }
-});
-
-function wordcheck(index){
+    divs[i-1].value = "";
+    i--;
+    return;
+}
+function wordCheck(index){
     iteration++;
     let letterMap = new Map();
-    let localCheck = true;
+    let localCheck = true;//check whether all characters match
+
     for(let i = 0 ; i < 5; i++){
         // divs[index].style.backgroundColor = '#B59F3B';
         // divs[index].style.backgroundColor = '#3A3A3C';
@@ -94,25 +131,33 @@ function wordcheck(index){
     for(let i = 0 ; i < 5; i++, index++){
         if(divs[index].value == word[i]){
             divs[index].style.backgroundColor = '#538D4E';
+            fillKey(divs[index].value, '#538D4E');
             letterMap.set(word[i],letterMap.get(word[i])-1);
         }
     }
+
     index-=5;
+
     for(let i = 0 ; i < 5; i++, index++){
         if(divs[index].value != word[i]){
             if(!letterMap.has(divs[index].value)){
                 divs[index].style.backgroundColor = '#3A3A3C';
+                fillKey(divs[index].value, '#3A3A3C');
             }
             else if(letterMap.get(divs[index].value) == 0){
                 divs[index].style.backgroundColor = '#3A3A3C';
+                fillKey(divs[index].value, '#3A3A3C');
             }
             else if(letterMap.get(divs[index].value) >= 1){
                 divs[index].style.backgroundColor = '#B59F3B';
+                fillKey(divs[index].value, '#B59F3B');
                 letterMap.set(divs[index].value, letterMap.get(divs[index].value) - 1);
             }
         }
     }
+
     index-=5;
+
     for(let i = 0 ; i < 5; i++, index++){
         if(divs[index].value != word[i]){
             localCheck = false;
@@ -132,10 +177,21 @@ function wordcheck(index){
         },1200);
     }
 }
-function findword(index){
+
+function findWord(index){
     let str = "";
     for(let i =0;i < 5;i++,index++){
         str += divs[index].value;
     }
     return list.includes(str.toLowerCase());
+}
+
+function fillKey(value, color){
+    const letters = document.querySelectorAll("#keyboard > div > div");
+        for(let letter of letters){
+            if(letter.textContent == value){
+                letter.style.backgroundColor = `${color}`;
+                break;
+            }
+        }
 }
